3d969326d71341d077987ef243a6f51fc98102ce
pimonj <porsha.contact@gmail.com>
2022-12-27 22:48:32 +0700

    commit code

--- expense_struct.go
+++ expense_struct.go
@@ -1,17 +1,16 @@
 package main
 
 type ReqExpense struct {
-	AMOUNT 		int 	`json:"amount"`
-	NOTE 		string  `json:"note"`
-	TAGS 		string  `json:"tags"`
-	TITLE 		string  `json:"title"`
+	AMOUNT 		float64 	 `json:"amount"`
+	NOTE 		string  	 `json:"note"`
+	TAGS 		[]string  	 `json:"tags"`
+	TITLE 		string   	 `json:"title"`
 }
 
-
 type ResExpense struct {
-	AMOUNT 		int 	`json:"amount"`
-	ID			string  `json:"id"`
-	NOTE 		string  `json:"note"`
-	TAGS 		string  `json:"tags"`
-	TITLE 		string  `json:"title"`
+	ID			string  	`json:"id"`
+	TITLE 		string      `json:"title"`
+	AMOUNT 		float64 	`json:"amount"`	
+	NOTE 		string  	`json:"note"`
+	TAGS 		[]string    `json:"tags"`	
 }
---
+++ server.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func main() {
+	var err error
+	url := os.Getenv("DATABASE_URL")
+	db, err = sql.Open("postgres", url)
+
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+
+	defer db.Close()
+
+	createTb := `CREATE TABLE IF NOT EXISTS expenses ( id SERIAL PRIMARY KEY, title TEXT, amount FLOAT, note TEXT, tags TEXT[] );`
+
+	_, err = db.Exec(createTb)
+	if err != nil {
+		log.Fatal("Can't create table", err)
+	}
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	port := os.Getenv("PORT")
+	if port != "2565" {
+		e.Logger.Fatal("Server should be start at port:2565", err)		
+	}
+
+	go func() {
+		if err := e.Start(":2565"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("Shutting down the server")
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+	<-shutdown
+	fmt.Println("shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
