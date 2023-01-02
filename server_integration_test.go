1390f5fa63a446b4b978360a4585f2e3498efa7b
pimonj <porsha.contact@gmail.com>
2023-01-02 21:39:55 +0700

    add integration test

---
+++ server_integration_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Response struct {
+	*http.Response
+	err error
+}
+
+func request(method, url string, body io.Reader) *Response {
+	req, _ := http.NewRequest(method, url, body)
+	req.Header.Add("Content-Type", "application/json")
+	client := http.Client{}
+	res, err := client.Do(req)
+	return  &Response{res, err}
+}
+
+func uri(paths ...string) string {
+	host := "http://localhost:2565"
+	if paths == nil {
+		return host
+	}
+
+	url := append([]string{host}, paths...)
+	return strings.Join(url, "/")
+
+}
+
+func TestGetExpenseByID(t *testing.T) {
+
+	var e ResExpense
+
+	res := request(http.MethodGet, uri("expenses",strconv.Itoa(1)), nil)
+	err := res.Decode(&e)
+
+	var tags = []string{
+		"food",
+		"beverage",
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+	assert.Equal(t, "1", e.ID)
+	assert.Equal(t, "night market promotion discount 10 bath", e.NOTE)
+	assert.Equal(t, float64(79), e.AMOUNT)
+	assert.Equal(t, "strawberry smoothie", e.TITLE)
+	assert.Equal(t, tags, e.TAGS)
+
+}
+
+
+func (r *Response) Decode(v interface{}) error {
+	if r.err != nil {
+		return r.err
+	}
+
+	return json.NewDecoder(r.Body).Decode(v)
+}
